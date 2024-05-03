d5ccf8275ec0045413c7710a5d5c6ade1fe1b252
Prince Kumar <109355387+Prince85219@users.noreply.github.com>
2024-05-04 01:23:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // Replace this with your login logic
+    alert('Login button clicked!');
+  });
+
+  // search bar
+document.getElementById("search_bar").addEventListener("click", function() {
+    var searchInputValue = document.getElementById("searchInput").value;
+    // You can replace this with your search logic
+    alert("Searching for: " + searchInputValue);
+  });
+  
